b0b8b1bcfad206718842a185850cbd5181415328
Thomas Loy <ths.loy@googlemail.com>
2016-07-11 02:40:09 +0200

    Teleport and format

--- src/main/java/com/winthier/adviceanimals/PlayerSession.java
+++ src/main/java/com/winthier/adviceanimals/PlayerSession.java
@@ -1,21 +1,24 @@
 package com.winthier.adviceanimals;
 
-import org.bukkit.entity.Player;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class PlayerSession {
-        public final String name;
-        public String selectAnimal = null;
-        public boolean info = false;
-        public long lastAdvice = 0L;
-        public AdviceAnimal lastAnimal;
-        public int adviceIndex;
+    public final String name;
+    public String selectAnimal = null;
+    public boolean info = false;
+    public String rename = null;
+    public Location teleport = null;
+    public long lastAdvice = 0L;
+    public AdviceAnimal lastAnimal;
+    public int adviceIndex;
 
-        public PlayerSession(Player player) {
-                name = player.getName();
-        }
+    public PlayerSession(Player player) {
+        name = player.getName();
+    }
 
-        public Player getPlayer() {
-                return Bukkit.getServer().getPlayerExact(name);
-        }
+    public Player getPlayer() {
+        return Bukkit.getServer().getPlayerExact(name);
+    }
 }
